84dd3bcb3b6d7813d57fe7d9ffe29c6d7307d81d
Rio Yokota <rioyokota@gsic.titech.ac.jp>
2022-04-12 17:40:34 +0900

    Uncomment private(a).

--- 05_openacc/05_scope.cpp
+++ 05_openacc/05_scope.cpp
@@ -4,7 +4,7 @@
 int main() {
   int a = 0;
   int b[1] = {0};
-#pragma acc parallel loop //private(a)
+#pragma acc parallel loop private(a)
   for(int i=0; i<8; i++) {
     a = __pgi_vectoridx();
     b[0] = __pgi_vectoridx();
