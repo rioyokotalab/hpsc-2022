4bd09a290db59a423a8b4b4ff2d872d19e5ba2a9
SupGilllll <56338743+SupGilllll@users.noreply.github.com>
2022-04-18 21:07:04 +0800

    Update 14_bucket_sort.cpp
    Assignment1

--- 02_openmp/14_bucket_sort.cpp
+++ 02_openmp/14_bucket_sort.cpp
@@ -1,32 +1,52 @@
 #include <cstdio>
 #include <cstdlib>
 #include <vector>
+#include <omp.h>
 
 int main() {
-  int n = 50;
-  int range = 5;
-  std::vector<int> key(n);
-  for (int i=0; i<n; i++) {
-    key[i] = rand() % range;
-    printf("%d ",key[i]);
-  }
-  printf("\n");
-
-  std::vector<int> bucket(range,0); 
-  for (int i=0; i<n; i++)
-    bucket[key[i]]++;
-  std::vector<int> offset(range,0);
-  for (int i=1; i<range; i++) 
-    offset[i] = offset[i-1] + bucket[i-1];
-  for (int i=0; i<range; i++) {
-    int j = offset[i];
-    for (; bucket[i]>0; bucket[i]--) {
-      key[j++] = i;
+    int n = 50;
+    int range = 5;
+    std::vector<int> key(n);
+    for (int i = 0; i < n; i++) {
+        key[i] = rand() % range;
+        printf("%d ", key[i]);
     }
-  }
+    printf("\n");
 
-  for (int i=0; i<n; i++) {
-    printf("%d ",key[i]);
-  }
-  printf("\n");
+    std::vector<int> bucket(range, 0);
+    for (int i = 0; i < n; i++)
+        bucket[key[i]]++;
+    std::vector<int> offset(range, 0);
+    std::vector<int> buffer(range, 0);
+#pragma omp parallel for
+    for (int j = 1; j < range; j++)
+    {
+        offset[j] = bucket[j - 1];
+    }
+#pragma omp parallel
+    for (int i = 1; i < range; i <<= 1) 
+    {
+#pragma omp for
+        for (int j = 0; j < range; j++)
+        {
+            buffer[j] = offset[j];
+        }
+#pragma omp for
+        for (int j = i; j < range; j++)
+        {
+            offset[j] += buffer[j - i];
+        }
+    }
+#pragma omp parallel for
+    for (int i = 0; i < range; i++) {
+        int j = offset[i];
+        for (; bucket[i] > 0; bucket[i]--) {
+            key[j++] = i;
+        }
+    }
+
+    for (int i = 0; i < n; i++) {
+        printf("%d ", key[i]);
+    }
+    printf("\n");
 }
