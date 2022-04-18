583c958fc7d98d8597bdc472b31de858e95ef37c
SupGilllll <56338743+SupGilllll@users.noreply.github.com>
2022-04-18 21:05:48 +0800

    Update 13_merge_sort.cpp
    Assignment1

--- 02_openmp/13_merge_sort.cpp
+++ 02_openmp/13_merge_sort.cpp
@@ -1,45 +1,53 @@
 #include <cstdio>
 #include <cstdlib>
 #include <vector>
+#include <omp.h>
 
 void merge(std::vector<int>& vec, int begin, int mid, int end) {
-  std::vector<int> tmp(end-begin+1);
-  int left = begin;
-  int right = mid+1;
-  for (int i=0; i<tmp.size(); i++) { 
-    if (left > mid)
-      tmp[i] = vec[right++];
-    else if (right > end)
-      tmp[i] = vec[left++];
-    else if (vec[left] <= vec[right])
-      tmp[i] = vec[left++];
-    else
-      tmp[i] = vec[right++]; 
-  }
-  for (int i=0; i<tmp.size(); i++) 
-    vec[begin++] = tmp[i];
+    std::vector<int> tmp(end - begin + 1);
+    int left = begin;
+    int right = mid + 1;
+    for (std::vector<int>::size_type i = 0; i < tmp.size(); i++) {
+        if (left > mid)
+            tmp[i] = vec[right++];
+        else if (right > end)
+            tmp[i] = vec[left++];
+        else if (vec[left] <= vec[right])
+            tmp[i] = vec[left++];
+        else
+            tmp[i] = vec[right++];
+    }
+    for (std::vector<int>::size_type i = 0; i < tmp.size(); i++)
+        vec[begin++] = tmp[i];
 }
 
 void merge_sort(std::vector<int>& vec, int begin, int end) {
-  if(begin < end) {
-    int mid = (begin + end) / 2;
-    merge_sort(vec, begin, mid);
-    merge_sort(vec, mid+1, end);
-    merge(vec, begin, mid, end);
-  }
+    if (begin < end) {
+        int mid = (begin + end) / 2;
+#pragma omp task shared(vec)
+        merge_sort(vec, begin, mid);
+#pragma omp task shared(vec)
+        merge_sort(vec, mid + 1, end);
+#pragma omp taskwait
+        merge(vec, begin, mid, end);
+    }
 }
 
 int main() {
-  int n = 20;
-  std::vector<int> vec(n);
-  for (int i=0; i<n; i++) {
-    vec[i] = rand() % (10 * n);
-    printf("%d ",vec[i]);
-  }
-  printf("\n");
-  merge_sort(vec, 0, n-1);
-  for (int i=0; i<n; i++) {
-    printf("%d ",vec[i]);
-  }
-  printf("\n");
+    int n = 20;
+    std::vector<int> vec(n);
+    for (int i = 0; i < n; i++) {
+        vec[i] = rand() % (10 * n);
+        printf("%d ", vec[i]);
+    }
+    printf("\n");
+#pragma omp parallel
+    {
+#pragma omp single
+        merge_sort(vec, 0, n - 1);
+    }  
+    for (int i = 0; i < n; i++) {
+        printf("%d ", vec[i]);
+    }
+    printf("\n");
 }
