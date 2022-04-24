4f2331c3cc5c90846855876f1461e77c1bcda797
Rio Yokota <rioyokota@gsic.titech.ac.jp>
2022-04-24 12:00:17 +0900

    Update OpenAcc

--- 05_openacc/06_iterator.cpp
+++ 05_openacc/06_iterator.cpp
@@ -1,13 +1,12 @@
 #include <cstdio>
 #include <vector>
-#include <openacc.h>
 
 int main() {
   std::vector<int> a(8);
   std::vector<int>::iterator it;
 #pragma acc parallel loop
   for(it=a.begin(); it<a.end(); it++) {
-    *it = __pgi_vectoridx();
+    *it = it-a.begin();
     printf("%d\n",*it);
   }
 }
--- 05_openacc/07_scope.cpp
+++ 05_openacc/07_scope.cpp
@@ -1,13 +1,12 @@
 #include <cstdio>
-#include <openacc.h>
 
 int main() {
   int a = 0;
   int b[1] = {0};
 #pragma acc parallel loop private(a)
   for(int i=0; i<8; i++) {
-    a = __pgi_vectoridx();
-    b[0] = __pgi_vectoridx();
+    a = i;
+    b[0] = i;
   }
   printf("%d %d\n",a,b[0]);
 }
