964db1d4adda10852d65489d605cb69112d4b450
Truewhiteshadow <93691532+Truewhiteshadow@users.noreply.github.com>
2022-04-28 05:30:50 +0900

    Update 11_distributed.cpp

--- 03_mpi/11_distributed.cpp
+++ 03_mpi/11_distributed.cpp
@@ -3,6 +3,8 @@
 #include <cmath>
 #include <mpi.h>
 
+//Student ID 20M28152
+
 struct Body {
   double x, y, m, fx, fy;
 };
@@ -29,6 +31,11 @@ int main(int argc, char** argv) {
   for(int irank=0; irank<size; irank++) {
     MPI_Send(jbody, N/size, MPI_BODY, send_to, 0, MPI_COMM_WORLD);
     MPI_Recv(jbody, N/size, MPI_BODY, recv_from, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+    MPI_Win win;
+    MPI_Win_create(jbody, N/size*sizeof(Body), sizeof(Body), MPI_INFO_NULL, MPI_COMM_WORLD, &win);
+    MPI_Win_fence(0, win);
+    MPI_Put(jbody, N/size, MPI_BODY, send_to, 0, N/size, MPI_BODY, win);
+    MPI_Win_fence(0, win);
     for(int i=0; i<N/size; i++) {
       for(int j=0; j<N/size; j++) {
         double rx = ibody[i].x - jbody[j].x;
