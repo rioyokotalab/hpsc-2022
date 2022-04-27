b9c056baba79b0c4eb85cf47723038ff06c66927
Eric-Zhang0 <69134576+Eric-Zhang0@users.noreply.github.com>
2022-04-27 22:19:38 +0800

    Update 13_nbody.cpp

--- 04_simd/13_nbody.cpp
+++ 04_simd/13_nbody.cpp
@@ -1,3 +1,5 @@
+// ZHANG HAOKE
+// Student ID: 22M31261
 #include <cstdio>
 #include <cstdlib>
 #include <cmath>
@@ -5,16 +7,17 @@
 
 int main() {
   const int N = 8;
-  float x[N], y[N], m[N], fx[N], fy[N], count[N];
+  float x[N], y[N], m[N], fx[N], fy[N], xtemp[N], ytemp[N];
   for(int i=0; i<N; i++) {
     x[i] = drand48();
     y[i] = drand48();
     m[i] = drand48();
-    fx[i] = fy[i] = 0;
+    fx[i] = fy[i] = xtemp[i] = ytemp[i] = 0;
   }
+
   for(int i=0; i<N; i++) {
-        __m256 J = _mm256_load_ps(count);
-        __m256 I = _mm256_set1_ps(count[i]);
+        __m256 J = _mm256_set_ps(0, 1, 2, 3, 4, 5, 6, 7);
+        __m256 I = _mm256_set1_ps(i);
         __m256 xivec = _mm256_set1_ps(x[i]);
         __m256 xjvec = _mm256_load_ps(x);
         __m256 rxvec = _mm256_sub_ps(xivec, xjvec);
@@ -22,10 +25,10 @@ int main() {
         __m256 yjvec = _mm256_load_ps(y);
         __m256 ryvec = _mm256_sub_ps(yivec, yjvec);
         
-        __m256 fxvec = _mm256_set1_ps(fx);
-        __m256 fyvec = _mm256_set1_ps(fy);
         __m256 mjvec = _mm256_load_ps(m);
         
+        __m256 fxvec = _mm256_setzero_ps();
+        __m256 fyvec = _mm256_setzero_ps();
         __m256 rxp2 = _mm256_mul_ps(rxvec, rxvec);
         __m256 ryp2 = _mm256_mul_ps(ryvec, ryvec);
         __m256 rvec = _mm256_rsqrt_ps(_mm256_add_ps(rxp2, ryp2));
@@ -39,8 +42,13 @@ int main() {
         __m256 mask = _mm256_cmp_ps(I, J, _CMP_NEQ_OQ);
         fxvec = _mm256_blendv_ps(fxvec, fxnew, mask);
         fyvec = _mm256_blendv_ps(fyvec, fynew, mask);
-        _mm256_store_ps(fx, fxnew);
-        _mm256_store_ps(fy, fynew);
+        _mm256_store_ps(xtemp, fxvec);
+        _mm256_store_ps(ytemp, fyvec);
+
+      for(int j = 0; j<N; j++){
+        fx[i] += xtemp[j];
+        fy[i] += ytemp[j];
+      }
     printf("%d %g %g\n",i,fx[i],fy[i]);
   }
 }
