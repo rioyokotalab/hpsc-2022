0210b2042d95b7a1d47631a044786dab35bc6f9a
Truewhiteshadow <93691532+Truewhiteshadow@users.noreply.github.com>
2022-05-08 20:11:31 +0900

    Update 14_bucket_sort.cpp

--- 02_openmp/14_bucket_sort.cpp
+++ 02_openmp/14_bucket_sort.cpp
@@ -1,6 +1,7 @@
 #include <cstdio>
 #include <cstdlib>
 #include <vector>
+#include <omp.h>
 
 int main() {
   int n = 50;
@@ -20,7 +21,7 @@ int main() {
   std::vector<int> offset(range,0);
   for (int i=1; i<range; i++) 
     offset[i] = offset[i-1] + bucket[i-1];
- #pragma omp parallel for 
+ #pragma omp parallel for private(i) private(j)
   for (int i=0; i<range; i++) {
     int j = offset[i];
     for (; bucket[i]>0; bucket[i]--) {
