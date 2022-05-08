cfe539ba750f2cc7ff0825c1167beebee6dde569
Rio Yokota <rioyokota@gsic.titech.ac.jp>
2022-05-08 22:56:53 +0900

    Simplify CUTLASS.

--- 08_cache/06_base.cu
+++ 08_cache/06_base.cu
@@ -1,50 +1,114 @@
-#include <cmath>
-#include <cstdlib>
-#include <cstdio>
+#include <iostream>
+#include <typeinfo>
+#include <random>
+#include <stdint.h>
+#include <cublas_v2.h>
 #include <chrono>
 using namespace std;
 
-__global__ void matmul(float *A, float *B, float *C, int N) {
-  int i = blockIdx.x;
-  int j = threadIdx.x;
-  float sum = 0;
-  for (int k=0; k<N; k++) {
-    sum += A[N*i+k] * B[N*k+j];
-  }
-  C[N*i+j] = sum;
-}
+__global__ void kernel(int dim_m, int dim_n, int dim_k,
+		       float *d_a, float *d_b, float *d_c) {
+  int offset_a_m = 64 * blockIdx.x;
+  int offset_b_n = 64 * blockIdx.y;
+  int m = threadIdx.x;
 
-int main(int argc, char **argv) {
-  int N = 1024;
-  int size = N * N * sizeof(float);
-  float *A, *B, *C;
-  cudaMallocManaged(&A, size);
-  cudaMallocManaged(&B, size);
-  cudaMallocManaged(&C, size);
-  for (int i=0; i<N; i++) {
-    for (int j=0; j<N; j++) {
-      A[N*i+j] = drand48();
-      B[N*i+j] = drand48();
-      C[N*i+j] = 0;
+  float block_c[64];
+
+  for (int n = 0; n < 64; ++n)
+    block_c[n] = 0;
+
+  for (int k = 0; k < dim_k; k += 8) {
+    int offset_a_k = k, offset_b_k = k;
+    for (int j = 0; j < 8; ++j) {
+      float block_a = d_a[(offset_a_k + j) * dim_m + offset_a_m + m];
+      for (int n = 0; n < 64; ++n) {
+        float block_b = d_b[(offset_b_n + n) * dim_k + offset_b_k + j];
+	block_c[n] += block_a * block_b;
+      }
     }
   }
+  for (int n = 0; n < 64; ++n) {
+    int c_n = offset_b_n + n;
+    int c_m = offset_a_m + m;
+    if (c_n < dim_n && c_m < dim_m) {
+      d_c[c_n * dim_m + c_m] = block_c[n];
+    }
+  }
+}
+
+int main(int argc, const char **argv) {
+  int m = 10240;
+  int k = 4096;
+  int n = 8192;
+  float alpha = 1.0;
+  float beta = 0.0;
+  int Nt = 10;
+  float *A, *B, *C, *C2;
+  cudaMallocManaged(&A, m * k * sizeof(float));
+  cudaMallocManaged(&B, k * n * sizeof(float));
+  cudaMallocManaged(&C, m * n * sizeof(float));
+  cudaMallocManaged(&C2, m * n * sizeof(float));
+  for (int i=0; i<m; i++)
+    for (int j=0; j<k; j++)
+      A[k*i+j] = drand48();
+  for (int i=0; i<k; i++)
+    for (int j=0; j<n; j++)
+      B[n*i+j] = drand48();
+  for (int i=0; i<n; i++)
+    for (int j=0; j<m; j++)
+      C[m*i+j] = C2[m*i+j] = 0;
+  cublasHandle_t cublas_handle;
+  cublasCreate(&cublas_handle);
   auto tic = chrono::steady_clock::now();
-  matmul<<<N,N>>>(A, B, C, N);
-  cudaDeviceSynchronize();
+  for (int i = 0; i < Nt+2; i++) {
+    if (i == 2) tic = chrono::steady_clock::now();
+    cublasSgemm(cublas_handle,
+		CUBLAS_OP_N,
+		CUBLAS_OP_N,
+		m,
+		n,
+		k,
+		&alpha,
+		A,
+		m,
+		B,
+		k,
+		&beta,
+		C,
+		m);
+    cudaDeviceSynchronize();
+  }
   auto toc = chrono::steady_clock::now();
-  double time = chrono::duration<double>(toc - tic).count();
-  printf("N=%d: %lf s (%lf GFlops)\n",N,time,2.*N*N*N/time/1e9);
-  #pragma omp parallel for
-  for (int i=0; i<N; i++)
-    for (int k=0; k<N; k++)
-      for (int j=0; j<N; j++)
-        C[N*i+j] -= A[N*i+k] * B[N*k+j];
+  int64_t num_flops = (2 * int64_t(m) * int64_t(n) * int64_t(k)) + (2 * int64_t(m) * int64_t(n));
+  double tcublas = chrono::duration<double>(toc - tic).count() / Nt;
+  double cublas_flops = double(num_flops) / tcublas / 1.0e9;
+  int tile = 64;
+  dim3 block = dim3(tile);
+  dim3 grid = dim3((m+tile-1)/tile, (n+tile-1)/tile);
+  for (int i = 0; i < Nt+2; i++) {
+    if (i == 2) tic = chrono::steady_clock::now();
+    kernel<<< grid, block >>>(m,
+			      n,
+			      k,
+			      A,
+			      B,
+			      C2);
+    cudaDeviceSynchronize();
+  }
+  toc = chrono::steady_clock::now();
+  double tcutlass = chrono::duration<double>(toc - tic).count() / Nt;
+  double cutlass_flops = double(num_flops) / tcutlass / 1.0e9;
+  printf("CUBLAS: %.2f Gflops, CUTLASS: %.2f Gflops\n", cublas_flops, cutlass_flops);
   double err = 0;
-  for (int i=0; i<N; i++)
-    for (int j=0; j<N; j++)
-      err += fabs(C[N*i+j]);
-  printf("error: %lf\n",err/N/N);
+  for (int i=0; i<n; i++) {
+    for (int j=0; j<m; j++) {
+      err += fabs(C[m*i+j] - C2[m*i+j]);
+    }
+  }
+  printf("error: %lf\n", err/n/m);
   cudaFree(A);
   cudaFree(B);
   cudaFree(C);
+  cudaFree(C2);
+  cublasDestroy(cublas_handle);
 }
