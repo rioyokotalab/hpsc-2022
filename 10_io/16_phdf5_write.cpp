a47e73218bc94fe56770ace6d31fe69353117170
Truewhiteshadow <93691532+Truewhiteshadow@users.noreply.github.com>
2022-05-23 07:44:34 +0900

    Update 16_phdf5_write.cpp

--- 10_io/16_phdf5_write.cpp
+++ 10_io/16_phdf5_write.cpp
@@ -15,11 +15,14 @@ int main (int argc, char** argv) {
   assert(mpisize == dim[0]*dim[1]);
   hsize_t N[2] = {NX, NY};
   hsize_t Nlocal[2] = {NX/dim[0], NY/dim[1]};
+  hsize_t Nlocal2[2] = {NX/dim[0]/2, NY/dim[1]/2};
   hsize_t offset[2] = {mpirank / dim[0], mpirank % dim[0]};
   for(int i=0; i<2; i++) offset[i] *= Nlocal[i];
   hsize_t count[2] = {1,1};
   hsize_t stride[2] = {1,1};
-  vector<int> buffer(Nlocal[0]*Nlocal[1],mpirank);
+  vector<int> buffer2(Nlocal2[0]*Nlocal2[1],mpirank);
+  vector<int> buffer;
+  for(int i=0; i<4; i++) buffer.insert(buffer.end(), buffer2.begin(), buffer2.end());
   hid_t plist = H5Pcreate(H5P_FILE_ACCESS);
   H5Pset_fapl_mpio(plist, MPI_COMM_WORLD, MPI_INFO_NULL);
   hid_t file = H5Fcreate("data.h5", H5F_ACC_TRUNC, H5P_DEFAULT, plist);
